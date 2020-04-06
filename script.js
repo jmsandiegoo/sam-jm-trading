a2a61f670f2bf26a903e85cc6cdaeaa96d9272c6
John Michael San Diego <jmsandiego0710@gmail.com>
2020-04-06 18:52:20 +0800

    Finshed product section

--- script.js
+++
@@ -1,6 +0,0 @@
-// Fix the 100vh problem
-window.onresize = function() {
-    var landingSection = document.querySelector(".landing-section");
-    landingSection.height = window.innerHeight;
-}
-window.onresize(); // called to initially set the height.
